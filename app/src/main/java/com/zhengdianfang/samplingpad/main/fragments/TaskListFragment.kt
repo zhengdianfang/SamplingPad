ed371e9f46589e3d465db5a75427764b00fc2f18
zhengdianfang <dfgzheng@thoughtworks.com>
2018-10-02 21:04:06 +0800

    build ui for task list

--- app/src/main/java/com/zhengdianfang/samplingpad/main/fragments/TaskListFragment.kt
+++ app/src/main/java/com/zhengdianfang/samplingpad/main/fragments/TaskListFragment.kt
@@ -2,14 +2,14 @@ package com.zhengdianfang.samplingpad.main.fragments
 
 
 import android.os.Bundle
-import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 
 import com.zhengdianfang.samplingpad.R
+import me.yokeyword.fragmentation.SupportFragment
 
-class TaskListFragment : Fragment() {
+class TaskListFragment : SupportFragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
